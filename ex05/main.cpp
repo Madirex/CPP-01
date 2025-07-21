6243c070f6f9655514e1e42711febf28c7db3235
Madirex <angel@madirex.com>
2025-07-21 13:44:07 +0200

    upload ex05

---
+++ ex05/main.cpp
@@ -0,0 +1,28 @@
+#include "Harl.hpp"
+
+int main(void)
+{
+	Harl harl;
+
+	std::cout << "--- Testing DEBUG ---" << std::endl;
+	harl.complain("DEBUG");
+	std::cout << std::endl;
+
+	std::cout << "--- Testing INFO ---" << std::endl;
+	harl.complain("INFO");
+	std::cout << std::endl;
+
+	std::cout << "--- Testing WARNING ---" << std::endl;
+	harl.complain("WARNING");
+	std::cout << std::endl;
+
+	std::cout << "--- Testing ERROR ---" << std::endl;
+	harl.complain("ERROR");
+	std::cout << std::endl;
+
+	std::cout << "--- Testing INVALID ---" << std::endl;
+	harl.complain("INVALID_LEVEL");
+	std::cout << std::endl;
+
+	return 0;
+}
